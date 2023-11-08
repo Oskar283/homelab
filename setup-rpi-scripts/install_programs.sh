8ba4cd180b75b96629da069274f7b87bd451a711
Oskar <oskarrodholm@msn.com>
2023-11-08 18:44:19 +0000

    Migrate/hard-copy setup-rpi repo here

---
+++ setup-rpi-scripts/install_programs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Script aims to install programs that I deem useful to the pi
+# Installs latest of all as I do not need HA.
+
+
+apt -y install tmux vim git cmake
+apt -y install mosh #for better ssh
+
+if [ -d "~/dotfiles"]
+then
+    git clone https://github.com/Oskar283/dotfiles.git ~/dotfiles
+else
+    echo "dotfiles already cloned"
+fi
+
+/bin/bash /home/$USER/dotfiles/install # Install dotfiles
