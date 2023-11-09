0ce1d5df36348c36d6d6b3e6d472f22fc18d23a8
Oskar <oskarrodholm@msn.com>
2023-11-09 20:13:06 +0000

    Completely rewrite ./install_programs.sh, Mostly stolen from ansible-easy-vpn

--- setup-rpi-scripts/install_programs.sh
+++ setup-rpi-scripts/install_programs.sh
@@ -1,16 +1,52 @@
-#!/bin/bash
+#!/bin/bash -ux
 # Script aims to install programs that I deem useful to the pi
-# Installs latest of all as I do not need HA.
 
+# Discard stdin. Needed when running from an one-liner which includes a newline
+read -t 0.1 -n 10000 discard
 
-apt -y install tmux vim git cmake
-apt -y install mosh #for better ssh
+# Quit on error
+set -e
 
-if [ -d "~/dotfiles"]
-then
-    git clone https://github.com/Oskar283/dotfiles.git ~/dotfiles
+check_root() {
+# Check if the user is root or not
+if [[ $EUID -ne 0 ]]; then
+    SUDO='sudo -E -H'
 else
-    echo "dotfiles already cloned"
+  SUDO=''
+fi
+}
+
+install_dependencies_debian() {
+  REQUIRED_PACKAGES=(
+    sudo
+    tmux
+    vim
+    git
+    cmake
+    mosh
+  )
+
+  check_root
+  # Disable interactive apt functionality
+  export DEBIAN_FRONTEND=noninteractive
+  # Update apt database, update all packages and install Ansible + dependencies
+  $SUDO apt update -y;
+  yes | $SUDO apt-get -o Dpkg::Options::="--force-confold" -fuy dist-upgrade;
+  yes | $SUDO apt-get -o Dpkg::Options::="--force-confold" -fuy install "${REQUIRED_PACKAGES[@]}"
+  yes | $SUDO apt-get -o Dpkg::Options::="--force-confold" -fuy autoremove;
+  export DEBIAN_FRONTEND=
+}
+
+install_dependencies_debian
+
+export dir=$HOME/dotfiles
+if [ -d "$dir" ];
+then
+  pushd $dir
+  git pull
+  popd
+else
+  git clone https://github.com/Oskar283/dotfiles $dir
 fi
 
-/bin/bash /home/$USER/dotfiles/install # Install dotfiles
+/bin/bash $dir/install # Install dotfiles
