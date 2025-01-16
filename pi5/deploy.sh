ed9879b10e6005a9ae91b074cceb9dc45e03ad96
Oskar <oskarrodholm@msn.com>
2025-01-16 19:45:21 +0000

    adjust deploy script to new path

--- pi5/deploy.sh
+++ pi5/deploy.sh
@@ -4,7 +4,7 @@
 REPO_ROOT=$(git rev-parse --show-toplevel)
 
 # Path to the NFS mount (you will replace this with your actual mount point)
-MOUNT_FOLDER="$REPO_ROOT/pi5/homeassistant_config_and_data"  # Relative path to the mount config
+MOUNT_FOLDER="/mnt/homeassistant_config_and_data"  # Relative path to the mount config
 
 # Exit if we're not in a git repo
 if [ ! -d "$REPO_ROOT/.git" ]; then
@@ -14,16 +14,7 @@ fi
 
 # Ensure the folder exists
 if [ ! -d "$MOUNT_FOLDER" ]; then
-    echo "Error: '$MOUNT_FOLDER' folder not found in the repository."
-    exit 1
-fi
-
-# Check if the mount folder is within the git repo structure
-if [[ "$MOUNT_FOLDER" == "$REPO_ROOT"* ]]; then
-    # Get the relative path to the mount folder from the repo root
-    RELATIVE_PATH="${MOUNT_FOLDER#$REPO_ROOT/}"
-else
-    echo "Error: The mount folder is not inside the git repository."
+    echo "Error: '$MOUNT_FOLDER' folder not found"
     exit 1
 fi
 
@@ -31,7 +22,7 @@ fi
 FILES_TO_DEPLOY=$(git ls-files --exclude-standard | grep "^homeassistant_config/")
 
 # Ensure the destination folder exists
-DEST_PATH="$REPO_ROOT/$RELATIVE_PATH"
+DEST_PATH="$MOUNT_FOLDER"
 mkdir -p "$DEST_PATH"
 
 # Loop through each file and copy it to the mount config destination
